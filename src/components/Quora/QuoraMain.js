f8454c386873f72a8b200a1c138005873cbd3345
viren sureja <virensureja3@gmail.com>
2021-06-12 09:42:11 +0530

    routes updated

---
+++ src/components/Quora/QuoraMain.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Quora from './Quora';
+
+function QuoraMain() {
+    return (
+        <div className="App">
+            <Quora />
+        </div>
+    );
+}
+
+export default QuoraMain;
