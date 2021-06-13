9c41cd9174bd6420121ad7b032d9338c87ca8986
viren sureja <virensureja3@gmail.com>
2021-06-14 00:17:32 +0530

    key added to post and rendering every at the starting time

--- src/components/Quora/Feed.js
+++ src/components/Quora/Feed.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../css/Quora/Feed.css';
+import db from '../../firebase';
 import Post from './Post';
 import QuoraBox from './QuoraBox';
 
 function Feed() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        db.collection('questions')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot(snapshot =>
+                setPosts(
+                    snapshot.docs.map(doc => ({
+                        id: doc.id,
+                        questions: doc.data(),
+                    }))
+                )
+            );
+    }, []);
+
     return (
         <div className="feed">
             <QuoraBox />
-            <Post />
-            <Post />
+            {posts.map(({ id, questions }) => (
+                <Post
+                    key={id}
+                    Id={id}
+                    question={questions.question}
+                    imageUrl={questions.imageUrl}
+                    timestamp={questions.timestamp}
+                    users={questions.user}
+                />
+            ))}
         </div>
     );
 }
