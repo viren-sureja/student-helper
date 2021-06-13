0169c190eb0bf9d23b8bfaf85a99ecd00d9ff2f3
viren sureja <virensureja3@gmail.com>
2021-06-14 00:15:08 +0530

    reducer of question info added

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import questionReducer from '../features/questionSlice';
 import userReducer from '../features/userSlice';
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
+        question: questionReducer,
     },
 });
