ece553ae6e1d16b85950c6dbd2d61275509e009c
viren sureja <virensureja3@gmail.com>
2021-06-14 00:16:10 +0530

    modal for asking question added

--- src/components/Quora/QuoraNavbar.js
+++ src/components/Quora/QuoraNavbar.js
@@ -1,4 +1,5 @@
-import { Avatar, Button } from '@material-ui/core';
+import { Avatar, Button, Input } from '@material-ui/core';
+import { ExpandMore, Link } from '@material-ui/icons';
 import AssignmentTurnedInOutlinedIcon from '@material-ui/icons/AssignmentTurnedInOutlined';
 import FeaturedPlayListOutlinedIcon from '@material-ui/icons/FeaturedPlayListOutlined';
 import HomeIcon from '@material-ui/icons/Home';
@@ -6,22 +7,38 @@ import LanguageIcon from '@material-ui/icons/Language';
 import NotificationsOutlinedIcon from '@material-ui/icons/NotificationsOutlined';
 import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined';
 import SearchIcon from '@material-ui/icons/Search';
-import React from 'react';
+import firebase from 'firebase';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import { useSelector } from 'react-redux';
 import '../../css/Quora/QuoraNavbar.css';
 import { selectUser } from '../../features/userSlice';
-import { auth } from '../../firebase';
+import db, { auth } from '../../firebase';
+
+Modal.setAppElement('#root');
 
 function QuoraNavbar() {
     const user = useSelector(selectUser);
+    const [openModal, setOpenModal] = useState(false);
+    const [input, setInput] = useState('');
+    const [inputUrl, setInputUrl] = useState('');
 
-    const handleLogout = () => {
-        auth.signOut()
-            .then(() => {
-                // console.log('hello');
-                // nothing to do currently
-            })
-            .catch(err => alert(err));
+    const questionName = input;
+    const handleQuestion = e => {
+        e.preventDefault();
+        setOpenModal(false);
+
+        if (questionName) {
+            db.collection('questions').add({
+                user: user,
+                question: input,
+                imageUrl: inputUrl,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            });
+        }
+
+        setInput('');
+        setInputUrl('');
     };
 
     return (
@@ -52,10 +69,94 @@ function QuoraNavbar() {
             </div>
             <div className="qHeader__Rem">
                 <div className="qHeader__avatar">
-                    <Avatar src={user.photo} onClick={handleLogout} />
+                    <Avatar
+                        onClick={() => auth.signOut()}
+                        className="Avatar"
+                        src={
+                            user.photo
+                                ? user.photo
+                                : 'https://images-platform.99static.com//_QXV_u2KU7-ihGjWZVHQb5d-yVM=/238x1326:821x1909/fit-in/500x500/99designs-contests-attachments/119/119362/attachment_119362573'
+                        }
+                    />
                 </div>
                 <LanguageIcon />
-                <Button>Add Question</Button>
+                <Button onClick={() => setOpenModal(true)}>Add Question</Button>
+
+                {/* modal starts here */}
+                <Modal
+                    isOpen={openModal}
+                    onRequestClose={() => setOpenModal(false)}
+                    shouldCloseOnOverlayClick={false}
+                    style={{
+                        overlay: {
+                            width: 700,
+                            height: 600,
+                            backgroundColor: 'rgba(0,0,0,0.8)',
+                            zIndex: '1000',
+                            top: '50%',
+                            left: '50%',
+                            marginTop: '-300px',
+                            marginLeft: '-350px',
+                        },
+                    }}
+                >
+                    <div className="modal__title">
+                        <h5>Add Question</h5>
+                        <h5>Share Link</h5>
+                    </div>
+                    <div className="modal__info">
+                        <Avatar
+                            className="avatar"
+                            src={
+                                user.photo
+                                    ? user.photo
+                                    : 'https://images-platform.99static.com//_QXV_u2KU7-ihGjWZVHQb5d-yVM=/238x1326:821x1909/fit-in/500x500/99designs-contests-attachments/119/119362/attachment_119362573'
+                            }
+                        />
+                        <p>
+                            {user.displayName ? user.displayName : user.email}{' '}
+                            asked
+                        </p>
+                        <div className="modal__scope">
+                            <PeopleAltOutlinedIcon />
+                            <p>Public</p>
+                            <ExpandMore />
+                        </div>
+                    </div>
+                    <div className="modal__Field">
+                        <Input
+                            required
+                            value={input}
+                            onChange={e => setInput(e.target.value)}
+                            type="text"
+                            placeholder="Start your question with 'What', 'How', 'Why', etc. "
+                        />
+                        <div className="modal__fieldLink">
+                            <Link />
+                            <input
+                                value={inputUrl}
+                                onChange={e => setInputUrl(e.target.value)}
+                                type="text"
+                                placeholder="Optional: inclue a link that gives context"
+                            ></input>
+                        </div>
+                    </div>
+                    <div className="modal__buttons">
+                        <button
+                            className="cancle"
+                            onClick={() => setOpenModal(false)}
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            type="sumbit"
+                            onClick={handleQuestion}
+                            className="add"
+                        >
+                            Add Question
+                        </button>
+                    </div>
+                </Modal>
             </div>
         </div>
     );
