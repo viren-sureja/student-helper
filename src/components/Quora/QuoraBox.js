8a37f6aa6f4e364c36eba0155ab3432bfb68bad0
viren sureja <virensureja3@gmail.com>
2021-06-14 00:19:08 +0530

    handled the case of failing

--- src/components/Quora/QuoraBox.js
+++ src/components/Quora/QuoraBox.js
@@ -10,11 +10,20 @@ function QuoraBox() {
     return (
         <div className="quoraBox">
             <div className="quoraBox__info">
-                <Avatar src={user.photo} />
-                <h5>{user.displayName}</h5>
+                <Avatar
+                    src={
+                        user.photo
+                            ? user.photo
+                            : 'https://images-platform.99static.com//_QXV_u2KU7-ihGjWZVHQb5d-yVM=/238x1326:821x1909/fit-in/500x500/99designs-contests-attachments/119/119362/attachment_119362573'
+                    }
+                    className="quoraBox__infoAvatar"
+                />
+                <h5>{user.displayName ? user.displayName : user.email}</h5>
             </div>
             <div className="quoraBox__quora">
-                <p>What is your question or link</p>
+                <p>
+                    Feel free to ask any question by using button "Add Question"
+                </p>
             </div>
         </div>
     );
