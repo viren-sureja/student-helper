aaecc77d7c8129b47487e4bf6473c1749c300838
viren sureja <virensureja3@gmail.com>
2021-07-08 18:52:06 +0530

    new images added

--- src/components/Quora/SideBarOptions.js
+++ src/components/Quora/SideBarOptions.js
@@ -4,6 +4,56 @@ import '../../css/Quora/SideBarOptions.css';
 
 const topics = [
     {
+        category: 'Placement',
+        // imgSrc: 'https://qphs.fs.quoracdn.net/main-thumb-t-930-100-cbbsbwijdhpyzlpipejvqpiijhhoaday.jpeg',
+        // imgSrc: '',
+        imgSrc: 'https://teresas.ac.in/wp-content/uploads/2018/04/placement-services.png',
+    },
+    {
+        category: 'Academics',
+        // imgSrc: 'https://qphs.fs.quoracdn.net/main-thumb-t-996-100-bfZBQjeEenKKl8fcNY4tVv0FyArtB0Mb.jpeg',
+        imgSrc: 'https://thumbs.dreamstime.com/b/academic-academic-apprentice-college-degree-education-educational-institution-establishment-111559495.jpg',
+    },
+    {
+        category: 'Admission',
+        // imgSrc: 'https://qphs.fs.quoracdn.net/main-thumb-t-2177-100-JiR07D1TQSfeQzRvWXomVaY4Poj2f8Yb.jpeg',
+        imgSrc: 'https://cache.careers360.mobi/media/presets/900X562/article_images/2021/6/3/admissioncounselling.jpeg',
+    },
+    {
+        category: 'General Info',
+        // imgSrc: 'https://qphs.fs.quoracdn.net/main-thumb-t-843-100-W7FzODceTO2aQmp8D7E4rKZ8YgSv21eR.jpeg',
+        imgSrc: 'https://www.ttsystem.com/sites/default/files/styles/full_image/public/info_contact.png?itok=O4Wy6NdX',
+    },
+    {
+        category: 'Fest',
+        // imgSrc: 'https://qphs.fs.quoracdn.net/main-thumb-t-931-100-c8WCPwZ9qPsh5zLGQ5wHh1ddxtc9Cch7.jpeg',
+        imgSrc: 'https://whataftercollege.com/wp-content/uploads/2019/03/cover-image-1-1024x535.jpg',
+    },
+];
+
+function SideBarOptions() {
+    return (
+        <div className="sidebarOptions">
+            {topics.map(topic => (
+                <div key={topic.category} className="sidebarOption">
+                    <img src={topic.imgSrc} alt="" />
+                    <p>{topic.category}</p>
+                </div>
+            ))}
+
+            <div className="sidebarOption">
+                <AddIcon />
+                <p className="text">Discover Spaces</p>
+            </div>
+        </div>
+    );
+}
+
+export default SideBarOptions;
+
+/*  
+const topics = [
+    {
         category: 'History',
         imgSrc: 'https://qphs.fs.quoracdn.net/main-thumb-t-930-100-cbbsbwijdhpyzlpipejvqpiijhhoaday.jpeg',
     },
@@ -36,23 +86,4 @@ const topics = [
         imgSrc: 'https://qphs.fs.quoracdn.net/main-thumb-t-877-100-e7jKHEQr0HExAIA9rlsyHlV6HJyRruEo.jpeg',
     },
 ];
-
-function SideBarOptions() {
-    return (
-        <div className="sidebarOptions">
-            {topics.map(topic => (
-                <div key={topic.category} className="sidebarOption">
-                    <img src={topic.imgSrc} alt="" />
-                    <p>{topic.category}</p>
-                </div>
-            ))}
-
-            <div className="sidebarOption">
-                <AddIcon />
-                <p className="text">Discover Spaces</p>
-            </div>
-        </div>
-    );
-}
-
-export default SideBarOptions;
+*/
