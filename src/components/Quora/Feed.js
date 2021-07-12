75414338f312340823200e4943359d730af8cde7
ShivamBhalodia <shivambhalodia007@gmail.com>
2021-07-12 16:35:13 +0530

    BugSolved

--- src/components/Quora/Feed.js
+++ src/components/Quora/Feed.js
@@ -1,84 +1,88 @@
-import React, { useEffect, useState } from 'react';
-import '../../css/Quora/Feed.css';
-import db from '../../firebase';
-import Post from './Post';
+import React, { useEffect, useState } from "react";
+import "../../css/Quora/Feed.css";
+import db from "../../firebase";
+import Post from "./Post";
 
 function Feed({ tag, university, handleTag }) {
-    // let tempTag = tag;
-    const [posts, setPosts] = useState([]);
-    const [dummyPosts, setDummyPosts] = useState([]);
+  // let tempTag = tag;
+  const [posts, setPosts] = useState([]);
+  const [dummyPosts, setDummyPosts] = useState([]);
 
-    const checkTag = tags => {
-        for (let i = 0; i < tags.length; i++) {
-            let currTag = tags[i];
-            if (currTag === tag) {
-                // console.log(currTag, tag);
-                return true;
-            }
-            // else console.log('------ unused -------', currTag);
+  const checkTag = (tags) => {
+    for (let i = 0; i < tags.length; i++) {
+      let currTag = tags[i];
+      if (currTag === tag) {
+        // console.log(currTag, tag);
+        return true;
+      }
+      // else console.log('------ unused -------', currTag);
+    }
+    return false;
+  };
+
+  const checkUniv = (univ) => {
+    return univ === university;
+  };
+
+  const checkArray = (newPosts) => {
+    console.log("current tag is:", tag);
+    let updatedPost = [];
+    // eslint-disable-next-line array-callback-return
+    newPosts.map((post) => {
+      if (
+        (tag === "" || checkTag(post.questions.tags)) &&
+        (university === "" || checkUniv(post.questions.univComp))
+      ) {
+        updatedPost.push(post);
+      }
+    });
+    console.log(updatedPost);
+    return updatedPost;
+  };
+
+  useEffect(() => {
+    console.log(typeof tag, tag, tag === "");
+    db.collection("questions")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        console.log("Sanpshot");
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            questions: doc.data(),
+          }))
+        );
+        if (tag === "") {
+          handleTag("all");
         }
-        return false;
-    };
+      });
+    console.log(posts);
+    if (tag !== "") {
+      setDummyPosts(checkArray(posts));
 
-    const checkUniv = univ => {
-        return univ === university;
-    };
+      console.log("hello");
+    } else setDummyPosts(posts);
 
-    const checkArray = newPosts => {
-        console.log('current tag is:', typeof tag);
-        let updatedPost = [];
-        // eslint-disable-next-line array-callback-return
-        newPosts.map(post => {
-            if (
-                (tag === '' || checkTag(post.questions.tags)) &&
-                (university === '' || checkUniv(post.questions.univComp))
-            ) {
-                updatedPost.push(post);
-            }
-        });
-        console.log(updatedPost);
-        return updatedPost;
-    };
+    console.log("HIIIiiii");
+  }, [tag, university]);
 
-    useEffect(() => {
-        console.log(typeof tag, tag, tag === '');
-        db.collection('questions')
-            .orderBy('timestamp', 'desc')
-            .onSnapshot(snapshot => {
-                // console.log(snapshot);
-                setPosts(
-                    snapshot.docs.map(doc => ({
-                        id: doc.id,
-                        questions: doc.data(),
-                    }))
-                );
-            });
-        console.log(posts);
-        if (tag !== '') {
-            setDummyPosts(checkArray(posts));
-            console.log('hello');
-        } else setDummyPosts(posts);
-
-        console.log(dummyPosts);
-    }, [tag, university]);
-
-    return (
-        <div className="feed">
-            {/* <QuoraBox /> */}
-            {dummyPosts.map(({ id, questions }) => (
-                <Post
-                    key={id}
-                    Id={id}
-                    question={questions.question}
-                    imageUrl={questions.imageUrl}
-                    timestamp={questions.timestamp}
-                    users={questions.user}
-                    upVote={questions.upVote}
-                    downVote={questions.downVote}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div className="feed">
+      {/* <QuoraBox /> */}
+      {dummyPosts.map(({ id, questions }) => (
+        <Post
+          key={id}
+          Id={id}
+          question={questions.question}
+          imageUrl={questions.imageUrl}
+          timestamp={questions.timestamp}
+          users={questions.user}
+          upVote={questions.upVote}
+          downVote={questions.downVote}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Feed;
--- src/components/Quora/Quora.js
+++ src/components/Quora/Quora.js
@@ -1,34 +1,34 @@
-import React, { useState } from 'react';
-import '../../css/Quora/Quora.css';
-import Feed from './Feed';
-import QuoraNavbar from './QuoraNavbar';
-import Sidebar from './Sidebar';
+import React, { useState } from "react";
+import "../../css/Quora/Quora.css";
+import Feed from "./Feed";
+import QuoraNavbar from "./QuoraNavbar";
+import Sidebar from "./Sidebar";
 
 function Quora() {
-    const [tag, setTag] = useState('');
-    const [university, setUniversity] = useState('');
+  const [tag, setTag] = useState("");
+  const [university, setUniversity] = useState("");
 
-    const handleTag = currTag => {
-        setUniversity('');
-        setTag(currTag);
-    };
+  const handleTag = (currTag) => {
+    setUniversity("");
+    setTag(currTag);
+  };
 
-    const handleUniversity = univ => {
-        console.log(univ);
-        setTag('all');
-        setUniversity(univ);
-    };
+  const handleUniversity = (univ) => {
+    console.log(univ);
+    setTag("all");
+    setUniversity(univ);
+  };
 
-    return (
-        <div className="quora">
-            <QuoraNavbar handleUniversity={handleUniversity} />
-            <div className="quora__content">
-                <Sidebar handleTag={handleTag} />
-                <Feed tag={tag} university={university} handleTag={handleTag} />
-                {/* <Widget /> */}
-            </div>
-        </div>
-    );
+  return (
+    <div className="quora">
+      <QuoraNavbar handleUniversity={handleUniversity} />
+      <div className="quora__content">
+        <Sidebar handleTag={handleTag} />
+        <Feed tag={tag} university={university} handleTag={handleTag} />
+        {/* <Widget /> */}
+      </div>
+    </div>
+  );
 }
 
 export default Quora;
